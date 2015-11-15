f7b97b284500d5b7ff7b78ecbc702c65451c8a60
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2015-11-15 11:25:18 +0100

    Completed task part 3 (no graphs)

--- experiment.cpp
+++ experiment.cpp
@@ -4,6 +4,7 @@
 #include "TH3.h"
 #include "TCanvas.h"
 #include "TRandom3.h"
+#include "TVector2.h"
 #include "TVector3.h"
 #include "TLorentzVector.h"
 
@@ -18,6 +19,16 @@ const double Pi_mass          /*GeV*/ =   0.139570; // charged pi
 
 const double Pi_energy_cm = K_mass/2;
 const double Pi_momentum_mod_cm = sqrt(Pi_energy_cm*Pi_energy_cm - Pi_mass*Pi_mass);
+
+/* Detector coordinates */
+const double z1 /* m */ = 50;
+const double z2 /* m */ = 51;
+const double z3 /* m */ = 60;
+const double z4 /* m */ = 61;
+
+/* Detector smearing */
+const double dx /* m */ =  0.01;
+const double dy /* m */ =  0.01;
 
 const int N_events = 1e5;
 
@@ -47,6 +58,20 @@ inline TVector3 generate_Pi_momentum_cm()
     return TVector3(x, y, z);
 }
 
+inline TVector2 chamber_hit(double z_chamber, double z_start, TVector3 Pi_momentum)
+{
+    double z_travelled = z_chamber - z_start;
+    double scale = z_travelled/Pi_momentum.Pz();
+    return TVector2(Pi_momentum.Px()*scale, Pi_momentum.Py()*scale);
+}
+
+inline TVector2 apply_chamber_smearing(TVector2& hit, double dx, double dy)
+{
+    double x = rng.Gaus(hit.x(), dx);
+    double y = rng.Gaus(hit.y(), dy);
+    return TVector2(x, y);
+}
+
 int experiment()
 {
     rng = TRandom3(12345); /* Fixed init */
@@ -74,23 +99,43 @@ int experiment()
     // Main loop
     
     for (int nev=0; nev<N_events; ++nev) {
+        
+        // Generate K energy
         double K_energy = generate_K_energy();
         histo_K_energy->Fill(K_energy);
         
+        // Generate K path
         double path_cm = generate_K_path();
         double path = gamma_from_K_energy(K_energy)*path_cm;
         histo_K_path->Fill(path);
         
+        // Discard K that have not decayed before the first chamber
+        if (path > z1) continue;
+        
+        // Generate cm dynamic
         double K_beta_z = sqrt(1-pow(K_mass/K_energy, 2));
         TVector3 K_beta = TVector3(0, 0, K_beta_z);
         TVector3 Pi_momentum_cm = generate_Pi_momentum_cm();
         histo_Pi_momentum_cm->Fill(Pi_momentum_cm.Px(), Pi_momentum_cm.Py(), Pi_momentum_cm.Pz());
         
+        // Boost to lab frame
         TLorentzVector Pi1 = TLorentzVector( Pi_momentum_cm, Pi_energy_cm);
         TLorentzVector Pi2 = TLorentzVector(-Pi_momentum_cm, Pi_energy_cm); // Momentum cons. in cm
         Pi1.Boost(K_beta);
         Pi2.Boost(K_beta);
         histo_Pi1->Fill(Pi1.Px(), Pi1.Py(), Pi1.Pz());
+        
+        // Calculate hit points in first two chambers
+        TVector2 hit11=chamber_hit(z1, path, Pi1.Vect());
+        TVector2 hit12=chamber_hit(z1, path, Pi2.Vect());
+        TVector2 hit21=chamber_hit(z2, path, Pi1.Vect());
+        TVector2 hit22=chamber_hit(z2, path, Pi2.Vect());
+        
+        // Apply chamber smearing
+        apply_chamber_smearing(hit11, dx, dy);
+        apply_chamber_smearing(hit12, dx, dy);
+        apply_chamber_smearing(hit21, dx, dy);
+        apply_chamber_smearing(hit22, dx, dy);
     }
     
     // Histogram drawing
